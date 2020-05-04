0980bc6436566df94010244c4f67ebebd5769c93
Jeff.A <kinesthesiac@gmail.com>
2020-05-03 20:52:04 -0700

    Further tuned the scraper to split the img strings and filter for ads.

--- dev/scrape.js
+++ dev/scrape.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 let path = "https://www.theonion.com/"
 //'https://api.nytimes.com/svc/search/v2/articlesearch.json??q=elections&api-key=sgaGlaiyYIsK8UZwDAPGfp5pmZbMfQyH'
 
+
 axios.get(path)
 .then((resp)=> {
     console.log("Success, returned from response");
@@ -14,14 +15,24 @@ axios.get(path)
     let $ = cheerio.load(resp.data);
 
     let articles = [];
-
+//article/a/source et
     $("article a").each(function(i, element) {
         let head = $(this).find("h4").text();
         let href = $(this).attr("href");
         let p = $(this).find('p').text();
-        console.log("headeer", head);
-        console.log("href=", href);
-        console.log("p:", p)
+        let image = $(this).find('img').attr("srcset");
+        //0 - with image, 1 - ad/skip, 2 - the title and content belonging to image
+        
+        if(image !== undefined) {
+            
+            console.log("href=", href);
+            console.log("headeer - with -image", head)
+            console.log("image: ", image.split(" ")[0])
+        } else {
+            console.log("headeer - no -image", head);
+            console.log("href=", href);
+            console.log("p:", p)
+        }
     })
 
     // let data = JSON.stringify(articles);
